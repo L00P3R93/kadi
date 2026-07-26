79c0ca8fc57b8e029cccd5bbf0a22836f15971ef
Vincent Kioko <vincentkioko51@gmail.com>
2026-07-26 13:21:41 +0300

    Preserve customer cache when updating profile pic
    Update the profile picture upload handler to merge the new profile picture into the existing cached customer data instead of replacing it entirely. This ensures other cached customer properties are retained after a profile picture update.

--- app/Http/Controllers/ProfilePictureController.php
+++ app/Http/Controllers/ProfilePictureController.php
@@ -26,12 +26,13 @@ class ProfilePictureController extends Controller
         try {
             $file = $request->file('photo');
             $ext = $file->getClientOriginalExtension() ?: 'jpg';
-            $filename = 'profile_'.$user->linked_id.'_'.time().'.'.$ext;
+            // $filename = 'profile_'.$user->linked_id.'_'.time().'.'.$ext;
 
             $response = app(KadiProfileImageService::class)
                 ->upload($user->linked_id, $file);
 
             $pic = "/{$user->linked_id}/".$response['data']['filename'];
+            $filename = $response['data']['filename'];
 
             // Update record in accounts table in kadi database
             DB::connection('kadi')
@@ -43,7 +44,10 @@ class ProfilePictureController extends Controller
                 ]);
 
             if (isset($response['data'])) {
-                Cache::put('kadi.customer.'.$user->id, $response['data'], now()->addHour());
+                $customer = Cache::get('kadi.customer.'.$user->id);
+                $customer['pic'] = $pic;
+
+                Cache::put('kadi.customer.'.$user->id, $customer, now()->addHour());
             }
 
             return back()->with('profile_success', 'Profile picture updated successfully.');
